37fb7e235eee03a8b7e98e6233c5bbaf2bdde27c
rahila1872 <rahilahafezi@gmail.com>
2025-08-06 10:18:59 +0000

    completed

--- basics/array1.js
+++ basics/array1.js
@@ -1,1 +1,12 @@
-console.log("rahila")
+var n1 = "rahila";
+var n2 = "laxmi";
+var n3 = "zansi";
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+
+var arr = ["rahila", "laxmi", "zansi"];
+console.log(arr[0]);
+console.log(arr[1]);
+console.log(arr[2]);
